62dae81ffe20c9df1ed97b282f63171e7eeb6ece
Vipul Kumar <tikateklabs@gmail.com>
2022-01-30 11:04:25 +0530

    Add Golu Bhaiya card

--- scripts/order_maker.py
+++ scripts/order_maker.py
@@ -2,12 +2,13 @@ import uuid as id
 import logging
 import os
 import pyqrcode
-import png
-from pyqrcode import QRCode
+
+print("""https://jeromeetienne.github.io/AR.js/three.js/examples/marker-training/examples/generator.html""")
+
 
 logging.info("Generating a new UUID")
-# uuid = id.uuid4()
-uuid = "12c0b1f3-4f89-4ae0-83f5-b85f496e698a"
+uuid = id.uuid4()
+# uuid = "12c0b1f3-4f89-4ae0-83f5-b85f496e698a"
 uuid = str(uuid)
 print(f"UUID: {str(uuid)}")
 logging.info(f"UUID generated : {str(uuid)}")
@@ -16,6 +17,10 @@ logging.info("Creating a folder inside magic_window")
 path_parent = os.path.dirname(os.getcwd())
 target_dir = os.path.join(path_parent, "magic_window", str(uuid))
 os.makedirs(target_dir, exist_ok=True)
+
+comments = input("Add comments for this order:")
+with open("order_info.txt", 'a') as info:
+    info.write(f"{uuid} | {comments}\n")
 
 # Read in the file
 with open('template.html', 'r') as template_file:
@@ -36,14 +41,12 @@ video_file = input("Enter the video file path:")
 os.rename(patt_file, os.path.join(target_dir, f"{uuid}.patt"))
 os.rename(marker_file, os.path.join(target_dir, f"{uuid}.PNG"))
 os.rename(video_file, os.path.join(target_dir, f"{uuid}.mp4"))
+os.rename("card_inside.docx", os.path.join(target_dir, "card_inside.docx"))
+os.rename("card_outside.docx", os.path.join(target_dir, "card_outside.docx"))
 
-web_url = f"https://tikateklabs.github.io/greetAR/magic_window/{uuid}/view.html"
+web_url = f"https://tikateklabs.com/greetAR/magic_window/{uuid}/view.html"
 # Generate QR code
 url = pyqrcode.create(web_url)
 
 # Create and save the png file naming "myqr.png"
 url.png(os.path.join(target_dir, f'QR_{uuid}.png'), scale=6)
-
-comments = input("Add comments for this order:")
-with open("order_info.txt", 'a') as info:
-    info.write(f"{uuid} | {comments}\n")
